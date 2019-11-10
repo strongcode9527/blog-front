938351aa1e8dcb6d83918f8fc6c587e9064b1f29
strongcode9527 <1477869317@qq.com>
2019-11-10 10:02:56 +0800

    :tada:

---
+++ scripts/dev.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const devServer = require('webpack-dev-server')
+const config = require('../webpack/webpack.dev')
+const openBrowser = require('react-dev-utils/openBrowser')
+const { choosePort, createCompiler, prepareUrls } = require('react-dev-utils/WebpackDevServerUtils')
+
+choosePort('localhost', 9527)
+.then(port => {
+
+  const urls = prepareUrls('http', 'localhost', port)
+  const params = {
+    webpack,
+    config,
+    appName: 'speed',
+    urls
+  }
+  const compiler = createCompiler(params);
+  console.log('in then')
+  const server = new devServer(compiler, {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost'
+  })
+
+  server.listen(port, 'localhost', () => {
+    console.log(`the client is open in ${port}`)
+    openBrowser(`http://localhost:${port}`)
+  })
+
+})  
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Speed, {Component, render} from 'strong-speed'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      num: 0,
+      show: true
+    }
+
+  }
+
+  componentDidMount() {
+    // this.id = setInterval(() => {
+    //   this.setState({num: this.state.num + 1, show: !this.state.show})
+    // }, 1)
+  }
+
+  componentDidUpdate() {
+    console.log('in update')
+  }
+
+  handleClick = () => {
+    console.log('insssss');
+  }
+
+  render() {
+    const {num} = this.state
+
+    return (
+      <div onClick={() => {console.log('in')}} style={{background: 'red'}} className="container">
+        <span>{num}</span> 
+        <span data-index={this.state.num}>strong2</span>
+        {
+          this.state.show && <div>show</div>
+        }  
+      </div>
+    )
+  }
+}
+
+render(<App aa="aaa"></App>, document.getElementById('root'))
